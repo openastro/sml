bc97d0b186208cecf54d5e2d3fdc250efd2b42b7
Kartik Kumar <me@kartikkumar.com>
2015-12-21 12:25:02 +0100

    Comment out bison and mscgen in doxygen update script.

--- scripts/update_doxygen.sh
+++ scripts/update_doxygen.sh
@@ -7,8 +7,8 @@ set -ev
 
 # Uninstall doxygen install. Fetch and build updated version from source.
 #yes | sudo apt-get --purge remove doxygen;
-sudo apt-get install bison;
-sudo apt-get install mscgen;
+#sudo apt-get install bison;
+#sudo apt-get install mscgen;
 git clone https://github.com/doxygen/doxygen.git;
 cd doxygen;
 ./configure;
