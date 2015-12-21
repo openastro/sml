081c326eb9d0aab7fbafdf04f69b82a6ed376bb0
Kartik Kumar <me@kartikkumar.com>
2015-12-21 12:18:48 +0100

    Update doxygen scripts.
    Update Travis config file.
    Make corrections in readme file.

--- scripts/update_doxygen.sh
+++ scripts/update_doxygen.sh
@@ -6,7 +6,7 @@
 set -ev
 
 # Uninstall doxygen install. Fetch and build updated version from source.
-yes | sudo apt-get --purge remove doxygen;
+#yes | sudo apt-get --purge remove doxygen;
 sudo apt-get install bison;
 sudo apt-get install mscgen;
 git clone https://github.com/doxygen/doxygen.git;
