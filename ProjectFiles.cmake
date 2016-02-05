bd05661632fb8ece5eb8ac6919729fde65e12741
Kartik Kumar <me@kartikkumar.com>
2016-02-06 02:18:23 +0530

    Create separate ProjectFiles.cmake file for easier management.
    Update description in readme, including file/folder structure section.
    Update Travis config script to use sudoless container.
    Add new bash scripts for CMake and Doxygen installation. Add new script to check Doxygen output in Travis script.

---
+++ ProjectFiles.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) 2014-2016 Kartik Kumar, Dinamica Srl (me@kartikkumar.com)
+# Distributed under the MIT License.
+# See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+
+# Set project test source files.
+set(TEST_SRC
+  "${TEST_SRC_PATH}/testSml.cpp"
+  "${TEST_SRC_PATH}/testBasicFunctions.cpp"
+  "${TEST_SRC_PATH}/testConstants.cpp"
+  "${TEST_SRC_PATH}/testLinearAlgebra.cpp"
+)
